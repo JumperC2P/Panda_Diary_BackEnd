af1200d6ee02ced5768d950c81e323ddbbbfa27b
ChemIsProgram <adslbbcc1949@gmail.com>
2020-03-27 13:15:24 +1100

    update cors settings

--- PandaDiary/src/main/java/rmit/sepm/PandaDiary/PandaDiaryApplication.java
+++ PandaDiary/src/main/java/rmit/sepm/PandaDiary/PandaDiaryApplication.java
@@ -2,7 +2,10 @@ package rmit.sepm.PandaDiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan
@@ -10,6 +13,16 @@ public class PandaDiaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PandaDiaryApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
 	}
 
 }
